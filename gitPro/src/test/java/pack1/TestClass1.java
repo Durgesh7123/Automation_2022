904981be294176ab3237b36dd256bf9e8adc4bcf
Durgesh7123 <118258983+Durgesh7123@users.noreply.github.com>
2022-11-15 15:34:29 +0530

    Update TestClass1.java

--- gitPro/src/test/java/pack1/TestClass1.java
+++ gitPro/src/test/java/pack1/TestClass1.java
@@ -2,14 +2,7 @@ package pack1;
 
 public class TestClass1 {
 	public static void main(String[] args) {
-		System.out.println("TC01");
-		System.out.println("TC01");
-		System.out.println("TC01");
-		System.out.println("TC01");
-		System.out.println("TC01");
-		System.out.println("TC01");
-		System.out.println("TC01");
-		System.out.println("TC01");
+		System.out.println("TC01");	
 	}
 
 }
